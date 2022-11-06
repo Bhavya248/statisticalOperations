a455f3a3cc81f8fe9c53feaeb81196112959443a
Bhavya248 <shah.bhavya248@gmail.com>
2022-11-06 10:26:36 +0000

    #c

--- Another Approch.py
+++ Another Approch.py
@@ -17,10 +17,10 @@ def input_data():
               observation.append(int(input()))
          print('Your entered data')
          print(observation)
-    '''End for Raw observations'''   
-    '''Ungrouped frequency distribution'''
-    if data_format == 'UGF' :
+         '''End for Raw observations'''   
+         '''Ungrouped frequency distribution'''
 
+    elif data_format == 'UGF' :
         for i in range(N):
               print('Enter the integral observation --> ')
               obs = int(input())
@@ -32,9 +32,9 @@ def input_data():
         print('Observation | Frequency')
         for i in range(N):
             print(observation[i],'|',frequency[i])
-    '''End for ungrouped frequency'''
-    '''Grouped frequency distribution'''
-    if data_format == 'GRF':
+        '''End for ungrouped frequency'''
+        '''Grouped frequency distribution'''
+    elif data_format == 'GRF':
         print('Enter class interval in following format -->  obs-obs')
         print(' ')
         for p in range(N):
@@ -56,15 +56,17 @@ def input_data():
         for i in range(N):
               print(observation[i][0],'-',observation[i][1],'    | ',frequency[i],'      | ',classmark[i])
               result = (classmark[i]*frequency[i])
+    else:
+        print('Enter valid data format !!')          
         return data_format      
  
 def operation(x,y) :
-    if x == 'MEAN' and y == 'raw' :
+    if (x == 'MEAN' and y == 'raw') :
         summation = 0
         for i in range(N):
             summation = summation + i
         print('Mean for given data is',summation/N)
-    elif x == 'MEAN' and y == 'UGF' :
+    elif (x == 'MEAN' and y == 'UGF') :
           for i in range(N):
                  print('Enter the integral observation --> ')
                  obs = int(input())
